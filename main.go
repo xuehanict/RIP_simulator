dee23962adb815af61524b09bf433a03d53f7c6b
xuehan <xuehan@ict.ac.cn>
2018-07-03 17:24:53 +0800

    RIP simulator done

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"fmt"
 	"sync"
+	"bufio"
+	"os"
 )
 
+type RouterID int
+
 type router struct {
-	ID         int
-	RouteTable routeTable
-	Update     chan *update
-	LinkedNodes map[int]struct{}
-	quit       chan int
+	ID           RouterID
+	RouteTable   routeTable
+	Update       chan *update
+	GlobalUpdate chan *update
+	quit         chan int
 }
 
 type routeTable struct {
@@ -18,42 +22,72 @@ type routeTable struct {
 }
 
 type routeTableEntry struct {
-	TargetId int
-	NextHop  int
-	//LinkId   int
+	TargetId RouterID
+	NextHop  RouterID
 	Distance int
-	//TODO(xuehan): add bindWidth
+	//TODO(xuehan): add bindWidth and link info
 }
 
 type update struct {
-	sourceRouterID int
+	sourceRouterID RouterID
+	targetRouterID RouterID
 	*routeTableEntry
 }
 
-func (r *router) sendUpdate(update *update, globalUpdateChan chan *update) error {
-	globalUpdateChan <- update
+func (r *router) sendUpdate(u *update) error {
+	r.GlobalUpdate <- u
 	return nil
 }
 
-func (r *router) handleUpdate(update *update) error {
-	targetID := update.TargetId
-	nextHop := update.NextHop
-	distance := update.Distance + 1
+func (r *router) handleUpdate(u *update) error {
+	targetID := u.TargetId
+	nextHop := u.sourceRouterID
+	distance := u.Distance + 1
 
 	if distance >= 16 {
 		distance = 16
 	}
 
+	isExist := false
 	for _, entry := range r.RouteTable.Entries {
+		// We find the entry, so just update it.
 		if entry.TargetId == targetID {
-			if entry.NextHop == nextHop{
+			isExist = true
+			if entry.NextHop == nextHop {
 				entry.Distance = distance
 			} else {
-				if entry.Distance > distance{
+				if entry.Distance > distance {
 					entry.NextHop = nextHop
 					entry.Distance = distance
 				}
 			}
+		}
+	}
+	// if this entry is not in the route table, we add this into table.
+	if !isExist {
+		newEntry := routeTableEntry{
+			TargetId: targetID,
+			NextHop:  nextHop,
+			Distance: distance,
+		}
+		r.RouteTable.Entries = append(r.RouteTable.Entries, newEntry)
+	}
+
+	// Broadcast this update to his neighbours
+	for _, entry := range r.RouteTable.Entries {
+		if entry.NextHop == entry.TargetId &&
+			entry.Distance == 1 &&
+			entry.TargetId != u.sourceRouterID {
+
+			update := &update{
+				sourceRouterID: r.ID,
+				targetRouterID: entry.TargetId,
+				routeTableEntry: &routeTableEntry{
+					TargetId: targetID,
+					Distance: distance,
+				},
+			}
+			r.sendUpdate(update)
 		}
 	}
 
@@ -76,13 +110,23 @@ func (r *router) stop() {
 	close(r.quit)
 }
 
+func newRouter(id RouterID, globalUpdate chan *update) *router{
+	newRouter := &router{
+		ID:           id,
+		Update:       make(chan *update),
+		RouteTable:   routeTable{},
+		GlobalUpdate: globalUpdate,
+		quit:		  make(chan int),
+	}
+	return newRouter
+}
+
 
 func main() {
-
-	var wg sync.WaitGroup
-	var quit chan int
-	var globalUpdateChan = make(chan *update, 10)
-	var routerMap map[int]*router
+	wg := sync.WaitGroup{}
+	quit := make(chan int)
+	globalUpdateChan := make(chan *update, 10)
+	routerMap := make(map[RouterID]*router)
 
 	wg.Add(1)
 	go func() {
@@ -90,7 +134,7 @@ func main() {
 		for {
 			select {
 			case update := <-globalUpdateChan:
-				router := routerMap[update.TargetId]
+				router := routerMap[update.targetRouterID]
 				router.Update <- update
 			case <-quit:
 				break
@@ -98,5 +142,18 @@ func main() {
 		}
 	}()
 
-	fmt.Printf("start server")
+	fmt.Printf("RIP简易版模拟器\n" +
+		"初始节点信息:\n" +
+		"1 <-----> 2 <----> 3 <-----> 4 \n" +
+		" |                         |    \n" +
+		" |-----------> 5 <---------| \n ")
+
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("输入1, 打印路由表" +
+			"输入2,添加节点" +
+			"输入3,添加链路" +
+			"输入其他无效作废")
+
+	}
 }
